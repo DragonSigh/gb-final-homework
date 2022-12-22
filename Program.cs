2e628e7705872159d292f36d71d9d1e149364a01
DragonSigh <dragonsigh@gmail.com>
2022-12-22 19:53:33 +0300

    added all the code in Program.cs

--- Program.cs
+++ Program.cs
@@ -7,3 +7,61 @@
 
 Console.Clear();
 
+int arraySize = GetNumberFromUser("Введите размер массива: ", "Ошибка ввода!");
+string[] myArray = new string[arraySize];
+FillArrayByUser(myArray);
+
+Console.WriteLine($"[“{String.Join("”, “", myArray) + "”"}] -> [“{String.Join("”, “", GetShortArray(myArray)) + "”"}]");
+
+//////////////////////////////////////////////////////////////////////////////////
+// Метод для получения массива из строк, длина которых меньше, либо равна 3 символам
+//////////////////////////////////////////////////////////////////////////////////
+static string[] GetShortArray(string[] inArray)
+{
+    int size = inArray.Length;
+    int count = 0;
+
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i].Length <= 3) count++;
+    }
+
+    string[] newArray = new string[count];
+    count = 0;
+
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i].Length <= 3)
+        {
+            newArray[count] = inArray[i];
+            count++;
+        }
+    }
+    return newArray;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+// Метод для ввода целого числа от пользователя
+//////////////////////////////////////////////////////////////////////////////////
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+// Метод заполнения строкового массива пользователем
+//////////////////////////////////////////////////////////////////////////////////
+static void FillArrayByUser(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива {i}: ");
+        array[i] = Console.ReadLine();
+    }
+}
